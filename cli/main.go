c58aec133e5f761591ba035e1d76f4a8e5ccd241
Mathieu Lecarme <mlecarme@bearstech.com>
2022-02-20 19:04:09 +0100

    Plug AuthRequest and Memory

--- cli/main.go
+++ cli/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"time"
+
+	authrequest "github.com/factorysh/chevillette/auth/auth_request"
 	"github.com/factorysh/chevillette/input/fluentd"
 	"github.com/factorysh/chevillette/log"
+	"github.com/factorysh/chevillette/memory"
 )
 
 func main() {
@@ -10,6 +15,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	f := fluentd.New("nginx", l.Log)
+	m := memory.New(context.TODO(), time.Hour)
+
+	f := fluentd.New("nginx", l.Log, m)
+	ar := authrequest.New(m)
+	go ar.ListenAndServe("0.0.0.0:8080")
 	f.ListenAndServe("0.0.0.0:24224")
 }
