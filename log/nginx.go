5c3f5fce00367498bdcb9543658389697c6fadaf
Mathieu Lecarme <mlecarme@bearstech.com>
2022-02-20 13:48:08 +0100

    No more globals, it's a struct.

--- log/nginx.go
+++ log/nginx.go
@@ -5,46 +5,45 @@ import (
 	iradix "github.com/hashicorp/go-immutable-radix"
 )
 
-var (
-	nginxLinePattern      pattern.Matcher
-	nginxLinePatternNames map[string]int
-)
-
-func init() {
-	var err error
-	nginxLinePattern, err = pattern.New(`<ip> - - <_> "<method> <url> <_>" <status> <_> <_> "<_>" <_>`)
-	if err != nil {
-		panic(err)
-	}
-	nginxLinePatternNames = make(map[string]int)
-	for i, name := range nginxLinePattern.Names() {
-		nginxLinePatternNames[name] = i
-	}
-}
-
 type Nginx struct {
-	tree *iradix.Node
+	tree             *iradix.Node
+	linePattern      pattern.Matcher
+	linePatternNames map[string]int
 }
 
-func NewNginx(prefix ...string) *Nginx {
+func NewNginx(prefix ...string) (*Nginx, error) {
 	r := iradix.New()
 	for _, p := range prefix {
 		r, _, _ = r.Insert([]byte(p), new(interface{}))
 	}
-	return &Nginx{
+	n := &Nginx{
 		tree: r.Root(),
 	}
+	err := n.SetPattern(`<ip> - - <_> "<method> <url> <_>" <status> <_> <_> "<_>" <_>`)
+	return n, err
+}
 
+func (n *Nginx) SetPattern(pttrn string) error {
+	var err error
+	n.linePattern, err = pattern.New(pttrn)
+	if err != nil {
+		return err
+	}
+	n.linePatternNames = make(map[string]int)
+	for i, name := range n.linePattern.Names() {
+		n.linePatternNames[name] = i
+	}
+	return nil
 }
 
 func (n *Nginx) Log(line []byte) (string, error) {
-	m := nginxLinePattern.Matches(line)
+	m := n.linePattern.Matches(line)
 	if len(m) == 0 { // the line doesn't match
 		return "", nil
 	}
 	_, _, ok := n.tree.LongestPrefix(m[2])
 	if ok {
-		return string(m[nginxLinePatternNames["ip"]]), nil
+		return string(m[n.linePatternNames["ip"]]), nil
 	}
 	return "", nil
 }
