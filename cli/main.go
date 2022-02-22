50aee1172219e880c861abab512f0065f4cd9adb
Mathieu Lecarme <mlecarme@bearstech.com>
2022-02-22 14:34:52 +0100

    Config file.

--- cli/main.go
+++ cli/main.go
@@ -2,23 +2,40 @@ package main
 
 import (
 	"context"
-	"time"
+	"os"
 
 	"github.com/factorysh/chevillette/auth/authrequest"
+	"github.com/factorysh/chevillette/conf"
 	"github.com/factorysh/chevillette/input/fluentd"
 	"github.com/factorysh/chevillette/log"
 	"github.com/factorysh/chevillette/memory"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	l, err := log.NewNginxLine("/")
+	cfgPath := os.Getenv("CONFIG")
+	if cfgPath == "" {
+		cfgPath = "/etc/chevillette.yml"
+	}
+	c, err := os.Open(cfgPath)
 	if err != nil {
 		panic(err)
 	}
-	m := memory.New(context.TODO(), time.Hour)
+	var cfg conf.Conf
+	err = yaml.NewDecoder(c).Decode(&cfg)
+	if err != nil {
+		panic(err)
+	}
+	cfg.Default()
+
+	l, err := log.NewNginxLine(cfg.Source.PrefixLines...)
+	if err != nil {
+		panic(err)
+	}
+	m := memory.New(context.TODO(), cfg.TTL)
 
 	f := fluentd.New("nginx", l.Log, m)
 	ar := authrequest.New(m)
-	go ar.ListenAndServe("0.0.0.0:8080")
-	f.ListenAndServe("0.0.0.0:24224")
+	go ar.ListenAndServe(cfg.AuthListen)
+	f.ListenAndServe(cfg.Source.FluentdListen)
 }
--- conf/conf.go
+++ conf/conf.go
@@ -3,12 +3,34 @@ package conf
 import "time"
 
 type Conf struct {
-	Source     Source        `yaml:"source"`
+	Source     *Source       `yaml:"source"`
 	TTL        time.Duration `yaml:"ttl"`
 	AuthListen string        `yaml:"auth_listen"`
+	WhiteList  []string      `yaml:"whitelist"`
 }
 
 type Source struct {
 	PrefixLines   []string `yaml:"prefix_lines"`
 	FluentdListen string   `yaml:"fluentd_listen"`
+}
+
+func (c *Conf) Default() {
+	if c.Source == nil {
+		c.Source = &Source{}
+	}
+	if c.Source.FluentdListen == "" {
+		c.Source.FluentdListen = "127.0.0.1:24224"
+	}
+	if c.Source.PrefixLines == nil {
+		c.Source.PrefixLines = []string{}
+	}
+	if c.TTL == 0 {
+		c.TTL = time.Hour
+	}
+	if c.AuthListen == "" {
+		c.AuthListen = "127.0.0.1:8080"
+	}
+	if c.WhiteList == nil {
+		c.WhiteList = []string{}
+	}
 }
