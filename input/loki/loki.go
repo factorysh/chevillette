15522e4931ed6723dbbcabe768dcb7f3268694dd
Mathieu Lecarme <mlecarme@bearstech.com>
2022-03-21 00:04:48 +0100

    Split large file.

--- input/loki/loki.go
+++ input/loki/loki.go
@@ -3,14 +3,11 @@ package loki
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	_url "net/url"
 	"time"
-
-	"strconv"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/websocket"
@@ -19,70 +16,6 @@ import (
 type Stream struct {
 	Stream  map[string]string `yaml:"stream"`
 	Entries []Entry           `yaml:"values"`
-}
-
-type Entry struct {
-	Timestamp time.Time
-	Line      string
-}
-
-type DroppedEntry struct {
-	Labels    map[string]string `yaml:"labels"`
-	Timestamp time.Time         `yaml:"timestamp"`
-}
-
-func (d *DroppedEntry) MarshalJSON() ([]byte, error) {
-	return json.Marshal(struct {
-		Timestamp string            `json:"timestamp"`
-		Labels    map[string]string `json:"labels"`
-	}{
-		Timestamp: fmt.Sprintf("%d", d.Timestamp.UnixNano()),
-		Labels:    d.Labels,
-	})
-}
-
-func (d *DroppedEntry) UnmarshalJSON(data []byte) error {
-	unmarshal := struct {
-		Timestamp string            `json:"timestamp"`
-		Labels    map[string]string `json:"labels"`
-	}{}
-	err := json.Unmarshal(data, &unmarshal)
-	if err != nil {
-		return err
-	}
-
-	t, err := strconv.ParseInt(unmarshal.Timestamp, 10, 64)
-	if err != nil {
-		return err
-	}
-	d.Timestamp = time.Unix(0, t)
-	d.Labels = unmarshal.Labels
-	return nil
-}
-
-func (e *Entry) MarshalJSON() ([]byte, error) {
-	return json.Marshal([]interface{}{
-		fmt.Sprintf("%d", e.Timestamp.UnixNano()),
-		e.Line,
-	})
-}
-
-func (e *Entry) UnmarshalJSON(data []byte) error {
-	l := make([]string, 0)
-	err := json.Unmarshal(data, &l)
-	if err != nil {
-		return err
-	}
-	if len(l) != 2 {
-		return fmt.Errorf("wrong entry length : %d", len(l))
-	}
-	t, err := strconv.ParseInt(l[0], 10, 64)
-	if err != nil {
-		return err
-	}
-	e.Timestamp = time.Unix(0, t)
-	e.Line = l[1]
-	return nil
 }
 
 type Tail struct {
