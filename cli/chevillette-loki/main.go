75bc73ea71397fc4de34778626e08828fc742f96
Mathieu Lecarme <mlecarme@bearstech.com>
2022-03-23 22:55:51 +0100

    Loki debug cli tool

--- cli/chevillette-loki/main.go
+++ cli/chevillette-loki/main.go
@@ -5,16 +5,21 @@ import (
 	"os"
 	"time"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/factorysh/chevillette/input/loki"
 )
 
 func main() {
 	u := os.Args[1]
-	l, err := loki.New(u)
+	l, err := loki.NewLoki(u)
 	if err != nil {
 		panic(err)
 	}
-	err = l.Tail(context.TODO(), `{job="nginx"}`, 5*time.Second, 42, time.Now().Add(-15*time.Minute))
+	err = l.Tail(context.TODO(), `{job="nginx"}`,
+		5*time.Second, 42, time.Now().Add(-15*time.Minute), func(data *loki.Tail) error {
+			spew.Dump(data)
+			return nil
+		})
 	if err != nil {
 		panic(err)
 	}
